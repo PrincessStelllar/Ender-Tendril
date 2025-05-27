71a7a87df9943dad126100f73291ad5d577730fb
SilentChaos512 <sworkman256@gmail.com>
2025-05-27 10:49:14 -0500

    Tweak block textures, add ender pearl recipe

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.21.5-1.8.0] - Unreleased
+- Ported to NeoForge 1.21.5
+- Tweak block textures
+- Added a recipe to craft ender pearls (for when other mods' recipes do not use tags correctly)
+
 ## [1.21.1-1.7.0] - 2024-10-19
 - Ported to NeoForge 1.21.1 (supports 1.21 too)
 - Added a config option to change the growth speed of ender tendrils
--- src/main/java/net/silentchaos512/endertendril/data/ModRecipesProvider.java
+++ src/main/java/net/silentchaos512/endertendril/data/ModRecipesProvider.java
@@ -1,17 +1,12 @@
 package net.silentchaos512.endertendril.data;
 
 import net.minecraft.core.HolderLookup;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.Items;
 import net.silentchaos512.endertendril.EnderTendrilMod;
 import net.silentchaos512.endertendril.setup.ModItems;
 import net.silentchaos512.lib.data.recipe.LibRecipeProvider;
-
-import java.util.concurrent.CompletableFuture;
 
 public class ModRecipesProvider extends LibRecipeProvider {
     public ModRecipesProvider(HolderLookup.Provider registries, RecipeOutput recipeOutput) {
@@ -25,5 +20,10 @@ public class ModRecipesProvider extends LibRecipeProvider {
                 .requires(Items.BLAZE_POWDER)
                 .unlockedBy("has_item", has(ModItems.TENDRIL_PEARL.get()))
                 .save(this.output, modId("ender_eye"));
+        shapeless(RecipeCategory.MISC, Items.ENDER_PEARL, 2)
+                .requires(Items.ENDER_PEARL)
+                .requires(ModItems.TENDRIL_PEARL)
+                .unlockedBy("has_item", has(ModItems.TENDRIL_PEARL.get()))
+                .save(this.output, modId("ender_pearl"));
     }
 }
